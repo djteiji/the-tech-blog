519575ee9924f432032229b5ce73379962575615
djteiji <djteiji@gmail.com>
2022-06-10 03:40:50 -0700

    login logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -62,7 +62,15 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-      .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
+      });
+    })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -70,32 +78,45 @@ router.post('/', (req, res) => {
   });
 
   router.post('/login', (req, res) => {
-
-    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
-        where: {
+      where: {
         email: req.body.email
-        }
+      }
     }).then(dbUserData => {
-        if (!dbUserData) {
+      if (!dbUserData) {
         res.status(400).json({ message: 'No user with that email address!' });
         return;
-    }
-
-    // res.json({ user: dbUserData });
-
-    // Verify user
-    const validPassword = dbUserData.checkPassword(req.body.password);
-    if (!validPassword) {
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
-      
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-
-   });  
   
+      req.session.save(() => {
+        // declare session variables
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    });
   });
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+
+ });
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
@@ -140,5 +161,8 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+
+
 
 module.exports = router;
